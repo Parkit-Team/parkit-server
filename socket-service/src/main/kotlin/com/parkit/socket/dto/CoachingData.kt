19cab71275cb8cdf4e3a7bde82501d22bfac332f
Yu Jin <qldls0307@naver.com>
2026-02-27 16:49:55 +0900

    chore: 주석 추가

--- socket-service/src/main/kotlin/com/parkit/socket/dto/CoachingData.kt
+++ socket-service/src/main/kotlin/com/parkit/socket/dto/CoachingData.kt
@@ -1,5 +1,13 @@
 package com.parkit.socket.dto
 
+/**
+ * 실시간 코칭 데이터
+ * @property sensorId 센서 ID
+ * @property distance 거리
+ * @property warningLevel 경고 레벨
+ * @property message 메시지
+ * @property timestamp 타임스탬프
+ */
 data class CoachingData(
     val sensorId: String,
     val distance: Double,
