f6470adaabf484b048ca25a0067f7b7936f03e2e
Yu Jin <qldls0307@naver.com>
2026-03-11 20:15:08 +0900

    chore: sensor 데이터 dto 실제 데이터 구조로 변경

--- analysis-service/src/main/kotlin/com/parkit/analysis/dto/ParkingSensorEvent.kt
+++ analysis-service/src/main/kotlin/com/parkit/analysis/dto/ParkingSensorEvent.kt
@@ -1,9 +1,21 @@
 package com.parkit.analysis.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
 /**
  * 센서 브릿지(Python)에서 Kafka를 통해 보내는 실시간 차량 센서 데이터를 표현하는 모델
  */
+
 data class ParkingSensorEvent(
-    val status: String,
-    val msg: String
+    val time: Double,
+    val x: Double,
+    val y: Double,
+    val z: Double,
+    val steer: Double,
+    @JsonProperty("wheel_degree") val wheelDegree: Double,
+    @JsonProperty("handle_angle") val handleAngle: Double,
+    val speed: Double,
+    @JsonProperty("front_dist") val frontDist: Double,
+    @JsonProperty("left_dist") val leftDist: Double,
+    @JsonProperty("right_dist") val rightDist: Double,
+    @JsonProperty("rear_dist") val rearDist: Double
 )
